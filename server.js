58a727376815d45b4f339844160d58a8cea70e30
Arka Jyoti Adhikary <arkajyotiadhikary15@gmail.com>
2022-07-27 15:18:05 +0530

    server updated

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use("/", authRoutes);
 app.use("/music", musicRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(".client/build/"));
+    app.use(express.static("./client/build/"));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
