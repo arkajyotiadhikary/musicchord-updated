4d8f2a5f78fb2d2037fbc32e6714634554564e9d
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2023-06-17 15:25:24 +0530

    move socket codes from node js to socket js file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const socketIO = require("socket.io");
 const http = require("http");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const initializeSocket = require("./socket");
 
 // Import routes
 const authRoutes = require("./router/auth");
@@ -23,64 +23,10 @@ mongoose.connect(
 
 const app = express();
 const server = http.createServer(app);
-const onlineUsers = {};
 
 // ------------------------------------------ socket -----------------------------------------------------
 // Socket io connection
-const io = socketIO(server, {
-    cors: {
-        origin: "localhost:3000",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true,
-    },
-});
-
-io.on("connection", (socket) => {
-    // adding new online user
-    onlineUsers[socket.id] = {
-        userID: socket.id,
-        username: "",
-    };
-
-    // store the conencted user to online user map
-
-    socket.on("username", (username) => {
-        onlineUsers[socket.id]["username"] = username;
-        console.log("Connected with username:", username);
-        io.emit("userConnected", Object.values(onlineUsers));
-        console.log("online users: ", Object.values(onlineUsers));
-    });
-    // io.emit("connection", updatedUserList());
-    socket.on("message", (data) => {
-        socket.broadcast.emit(
-            "client-message",
-            data,
-            socket.handshake.query.userName
-        );
-    });
-    socket.on("disconnect", () => {
-        // remove users as they left the server
-        delete onlineUsers[socket.id];
-        io.emit("userConnected", Object.values(onlineUsers));
-        // io.emit("disconnection", updatedUserList());
-    });
-});
-// socket io user list000
-// const addClientToMap = (socket) => {
-//     let userName = socket.handshake.query.userName;
-//     console.log(socket.handshake.query);
-//     if (!onlineUsers.has(socket.id)) {
-//         onlineUsers.set(socket.id, userName);
-//     }
-// };
-const removeClientFromMap = (socketID) => {
-    if (onlineUsers.has(socketID)) onlineUsers.delete(socketID);
-};
-const updatedUserList = () => {
-    console.log([...onlineUsers.values()]);
-    return [...onlineUsers.values()];
-};
+initializeSocket(server);
 
 // middlewares
 app.use(cors());
---
+++ socket.js
@@ -0,0 +1,61 @@
+const socketIO = require("socket.io");
+
+const intializeSocket = (server) => {
+    const onlineUsers = {};
+    const io = socketIO(server, {
+        cors: {
+            origin: "localhost:3000",
+            methods: ["GET", "POST"],
+            allowedHeaders: ["my-custom-header"],
+            credentials: true,
+        },
+    });
+
+    io.on("connection", (socket) => {
+        // adding new online user
+        onlineUsers[socket.id] = {
+            userID: socket.id,
+            username: "",
+        };
+
+        // store the conencted user to online user map
+
+        socket.on("username", (username) => {
+            onlineUsers[socket.id]["username"] = username;
+            console.log("Connected with username:", username);
+            io.emit("userConnected", Object.values(onlineUsers));
+            console.log("online users: ", Object.values(onlineUsers));
+        });
+        // io.emit("connection", updatedUserList());
+        socket.on("message", (data) => {
+            socket.broadcast.emit(
+                "client-message",
+                data,
+                socket.handshake.query.userName
+            );
+        });
+        socket.on("disconnect", () => {
+            // remove users as they left the server
+            delete onlineUsers[socket.id];
+            io.emit("userConnected", Object.values(onlineUsers));
+            // io.emit("disconnection", updatedUserList());
+        });
+    });
+    // socket io user list000
+    // const addClientToMap = (socket) => {
+    //     let userName = socket.handshake.query.userName;
+    //     console.log(socket.handshake.query);
+    //     if (!onlineUsers.has(socket.id)) {
+    //         onlineUsers.set(socket.id, userName);
+    //     }
+    // };
+    const removeClientFromMap = (socketID) => {
+        if (onlineUsers.has(socketID)) onlineUsers.delete(socketID);
+    };
+    const updatedUserList = () => {
+        console.log([...onlineUsers.values()]);
+        return [...onlineUsers.values()];
+    };
+};
+
+module.exports = intializeSocket;
